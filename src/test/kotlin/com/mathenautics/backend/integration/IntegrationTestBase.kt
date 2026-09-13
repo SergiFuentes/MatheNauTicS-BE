5b6515a88af39b45a34944331514005776468c07
SergiFuentes <sergifuentessanchez@gmail.com>
2026-09-14 00:07:23 +0200

    test: activate test profile with test JWT secret for context-loading tests

--- src/test/kotlin/com/mathenautics/backend/integration/IntegrationTestBase.kt
+++ src/test/kotlin/com/mathenautics/backend/integration/IntegrationTestBase.kt
@@ -2,9 +2,11 @@ package com.mathenautics.backend.integration
 
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.containers.PostgreSQLContainer
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 abstract class IntegrationTestBase {
 
     companion object {
